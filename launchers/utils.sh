3d6825d4a5f5d18369662ddefe36b230b2dc80d8
p0in-t <98182282+p0in-t@users.noreply.github.com>
2025-06-10 14:07:59 +0400

    Update utils.sh

--- launchers/utils.sh
+++ launchers/utils.sh
@@ -3,11 +3,11 @@ set -e
 source /environment.sh
 dt-launchfile-init
 
-echo "🚗 Starting lane follower..."
+echo "Starting lane follower..."
 
 dt-exec rosrun utils lane_follower.py
 dt-exec rosrun utils wheel_controller.py
 
-echo "🚗 Lane follower started!"
+echo "Lane follower started!"
 
 dt-launchfile-join
